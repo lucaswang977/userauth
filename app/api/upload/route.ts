52dc5dbcdd63b99790b2c603dc3025f70a9a238e
Lucas Wang <118956159+lucaswang977@users.noreply.github.com>
2023-12-05 18:39:39 +0900

    API can't be tested yet

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,17 +1,25 @@
-// https://github.com/vercel/examples/tree/main/solutions/aws-s3-image-upload
+// We simply save file in the public folder
+// https://codersteps.com/articles/building-a-file-uploader-from-scratch-with-next-js-app-directory
 
-// TODO: We can still use server action to calling S3's createPresigned api
+// TODO: Until now we cannot unit test the App router API using node-mock-http
 
+import { writeFile } from "fs/promises"
 import { decodeAndVerifyJwt } from "@/l/user"
 
 export async function POST(request: Request) {
-  const json = await request.json()
-  const { token } = json as { token: string }
-
-  const decoded = decodeAndVerifyJwt(token)
-  if (decoded) {
-    return Response.json({ result: true, userId: decoded.userId })
+  const formData = await request.formData()
+  const token = formData.get("token")
+  const file = formData.get("file") as Blob
+  // TODO: Check fingerprint
+  if (token && file) {
+    const decoded = decodeAndVerifyJwt(token.toString())
+    // TODO: Generate filename and url
+    if (decoded) {
+      const buffer = Buffer.from(await file.arrayBuffer())
+      const filename = `${Date.now()}-${Math.round(Math.random() * 1e9)}`
+      await writeFile(`public/${filename}`, buffer)
+      return Response.json({ result: true, file: filename })
+    }
   }
-
   return Response.json({ result: false }, { status: 400 })
 }
