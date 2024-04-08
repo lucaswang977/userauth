5f13f84ba3570d4bf51b5679b6bc65933d6d9aba
Lucas Wang <118956159+lucaswang977@users.noreply.github.com>
2024-04-08 02:08:20 +0000

    Small fixes on post create script

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Database initialize
 sudo service postgresql start
-psql -U postgres -c "CREATE ROLE puser LOGIN password 'localdbpassword';CREATE DATABASE testdb1 ENCODING 'UTF8' OWNER puser TEMPLATE template0;"
-npx cypress install
+psql -U postgres -c "CREATE ROLE puser LOGIN password 'localdbpassword';" -c "CREATE DATABASE testdb1 ENCODING 'UTF8' OWNER puser TEMPLATE template0;"
+
+# Dependencies intialize
+npx -y cypress install
+npm install -g pino-pretty
+
+# NPM initialize
+npm install
+npm run migrate
+npm run dbgen
