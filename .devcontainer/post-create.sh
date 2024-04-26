d7605f582ad667a2437c4adecf51dfba0521c0c7
Lucas Wang <118956159+lucaswang977@users.noreply.github.com>
2024-04-26 04:17:12 +0000

    Change the base image to the latest version

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,7 +2,7 @@
 
 # Database initialize
 sudo service postgresql start
-psql -U postgres -c "CREATE ROLE puser LOGIN password 'localdbpassword';" -c "CREATE DATABASE testdb1 ENCODING 'UTF8' OWNER puser TEMPLATE template0;"
+psql -U postgres -h 127.0.0.1 -c "CREATE ROLE puser LOGIN password 'localdbpassword';" -c "CREATE DATABASE testdb1 ENCODING 'UTF8' OWNER puser TEMPLATE template0;"
 
 # Dependencies intialize
 npx -y cypress install
